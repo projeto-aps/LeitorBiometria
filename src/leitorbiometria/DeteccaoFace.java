fdbfa933b0b093e0ef82d64eff28e9a6d520d0d3
DeboraBSilva <48605231+DeboraBSilva@users.noreply.github.com>
2021-10-26 19:50:26 -0300

    Reconhecimento

--- src/leitorbiometria/DeteccaoFace.java
+++ src/leitorbiometria/DeteccaoFace.java
@@ -4,7 +4,11 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.Base64;
 import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
@@ -12,11 +16,11 @@ import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
+import static org.opencv.highgui.Highgui.imwrite;
 import org.opencv.highgui.VideoCapture;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import util.TrainLBPH;
 
 public class DeteccaoFace extends javax.swing.JFrame {
 
@@ -27,6 +31,7 @@ public class DeteccaoFace extends javax.swing.JFrame {
     MatOfByte mem = new MatOfByte();
     CascadeClassifier faceDetector = new CascadeClassifier(DeteccaoFace.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
     MatOfRect faceDetections = new MatOfRect();
+    int numSamples = 25, sample = 1;
 
     class DaemonThread implements Runnable {
 
@@ -39,12 +44,40 @@ public class DeteccaoFace extends javax.swing.JFrame {
                     if (webSource.grab()) {
                         try {
                             webSource.retrieve(frame);
-                            Graphics g = jPanel1.getGraphics();
+                            Graphics g = pnlImagem.getGraphics();
                             faceDetector.detectMultiScale(frame, faceDetections);
                             for (Rect rect : faceDetections.toArray()) {
                                 Core.rectangle(frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
                                         new Scalar(0, 255, 0));
+                                Mat face = new Mat(frame, rect);
+
+                                if (btnCapturar.getModel().isPressed()) { //quando apertar o botão saveButton
+                                    if (txtNome.getText().equals("") || txtNome.getText().equals(" ")) {
+                                        JOptionPane.showMessageDialog(null, "Campo vazio");
+                                    } else {
+                                        if (sample <= numSamples) {
+//                                        salva a imagem cortada [160,160]
+//                                        nome do arquivo: idpessoa + a contagem de fotos. ex: person.10(id).6(sexta foto).jpg
+                                            String cropped = "C:\\photos\\person." + txtNome.getText() + "." + sample + ".jpg";
+                                            imwrite(cropped, face);
+
+                                            //System.out.println("Foto " + amostra + " capturada\n");
+//                                        counterLabel.setText(String.valueOf(sample) + "/25");
+                                            sample++;
+                                        }
+                                        if (sample > 25) {
+                                            new TrainLBPH().trainPhotos();//se a contagem for maior que 25, termina de tirar a foto, gera o arquivo
+//                                            insertDatabase(); //insere os dados no banco
+
+                                            System.out.println("File Generated");
+                                            stopCamera(); // e fecha a camera
+                                        }
+
+                                    }
+                                }
+
                             }
+
                             Highgui.imencode(".bmp", frame, mem);
                             Image im = ImageIO.read(new ByteArrayInputStream(mem.toArray()));
                             BufferedImage buff = (BufferedImage) im;
@@ -73,20 +106,23 @@ public class DeteccaoFace extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        pnlImagem = new javax.swing.JPanel();
         btnIniciar = new javax.swing.JButton();
         btnPausar = new javax.swing.JButton();
+        btnCapturar = new javax.swing.JButton();
+        lblNome = new javax.swing.JLabel();
+        txtNome = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout pnlImagemLayout = new javax.swing.GroupLayout(pnlImagem);
+        pnlImagem.setLayout(pnlImagemLayout);
+        pnlImagemLayout.setHorizontalGroup(
+            pnlImagemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 380, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        pnlImagemLayout.setVerticalGroup(
+            pnlImagemLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 380, Short.MAX_VALUE)
         );
 
@@ -104,6 +140,15 @@ public class DeteccaoFace extends javax.swing.JFrame {
             }
         });
 
+        btnCapturar.setText("Capturar");
+        btnCapturar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCapturarActionPerformed(evt);
+            }
+        });
+
+        lblNome.setText("Nome:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -112,23 +157,35 @@ public class DeteccaoFace extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(pnlImagem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(104, 104, 104)
                         .addComponent(btnIniciar)
                         .addGap(59, 59, 59)
-                        .addComponent(btnPausar)))
+                        .addComponent(btnPausar)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnCapturar))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(37, 37, 37)
+                        .addComponent(lblNome)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(pnlImagem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblNome)
+                    .addComponent(txtNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnIniciar)
-                    .addComponent(btnPausar))
+                    .addComponent(btnPausar)
+                    .addComponent(btnCapturar))
                 .addContainerGap())
         );
 
@@ -136,14 +193,15 @@ public class DeteccaoFace extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnPausarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPausarActionPerformed
+        stopCamera();
+    }//GEN-LAST:event_btnPausarActionPerformed
+
+    private void stopCamera() {
         myThread.runnable = false;            // stop thread
         btnPausar.setEnabled(false);   // activate start button 
         btnIniciar.setEnabled(true);     // deactivate stop button
-
         webSource.release();  // stop caturing fron cam
-
-
-    }//GEN-LAST:event_btnPausarActionPerformed
+    }
 
     private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIniciarActionPerformed
         webSource = new VideoCapture(0); // video capture from default cam
@@ -157,6 +215,10 @@ public class DeteccaoFace extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btnIniciarActionPerformed
 
+    private void btnCapturarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCapturarActionPerformed
+
+    }//GEN-LAST:event_btnCapturarActionPerformed
+
     public static void main(String args[]) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -167,8 +229,11 @@ public class DeteccaoFace extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCapturar;
     private javax.swing.JButton btnIniciar;
     private javax.swing.JButton btnPausar;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel lblNome;
+    private javax.swing.JPanel pnlImagem;
+    private javax.swing.JTextField txtNome;
     // End of variables declaration//GEN-END:variables
 }
---
+++ src/leitorbiometria/RecognizeFace.java
@@ -0,0 +1,265 @@
+package leitorbiometria;
+
+import java.awt.Desktop;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Array;
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.DoublePointer;
+import org.bytedeco.javacpp.IntPointer;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imencode;
+import org.bytedeco.opencv.global.opencv_imgproc;
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Rect;
+import org.bytedeco.opencv.opencv_core.RectVector;
+import org.bytedeco.opencv.opencv_core.Scalar;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_face.LBPHFaceRecognizer;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
+import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+
+public class RecognizeFace extends javax.swing.JDialog {
+
+    private RecognizeFace.DaemonThread myThread = null;
+
+    //JavaCV 1.5.1
+    VideoCapture webSource = null;
+    Mat cameraImage = new Mat();
+    CascadeClassifier cascade = new CascadeClassifier("C:\\photos\\haarcascade_frontalface_alt.xml");
+    LBPHFaceRecognizer recognizer = LBPHFaceRecognizer.create();
+
+    BytePointer mem = new BytePointer();
+    RectVector detectedFaces = new RectVector();
+
+    //Vars
+    String root, firstNamePerson, lastNamePerson, officePerson, dobPerson, telefone;
+    //Social Info
+    String facebook, insta, linkedin, git;
+    int idPerson;
+
+    //Utils
+    public RecognizeFace(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        recognizer.read("C:\\photos\\classifierLBPH.yml");
+        recognizer.setThreshold(80);
+        startCamera();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel10 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        label_photo = new javax.swing.JLabel();
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(95, 106, 117));
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel10.setText("Hi!");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setUndecorated(true);
+
+        jLabel3.setForeground(new java.awt.Color(82, 82, 82));
+        jLabel3.setText("Fullname:");
+
+        label_photo.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 255, 255)));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(jLabel3))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(label_photo, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel3)
+                .addGap(16, 16, 16)
+                .addComponent(label_photo, javax.swing.GroupLayout.PREFERRED_SIZE, 390, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        setSize(new java.awt.Dimension(820, 460));
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void sendMessage_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendMessage_btnActionPerformed
+    }//GEN-LAST:event_sendMessage_btnActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(RecognizeFace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(RecognizeFace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(RecognizeFace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(RecognizeFace.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the dialog */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                RecognizeFace dialog = new RecognizeFace(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel label_photo;
+    // End of variables declaration//GEN-END:variables
+
+    class DaemonThread implements Runnable {
+
+        protected volatile boolean runnable = false;
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                while (runnable) {
+                    try {
+                        if (webSource.grab()) {
+                            webSource.retrieve(cameraImage);
+                            Graphics g = label_photo.getGraphics();
+
+                            Mat imageGray = new Mat();
+                            cvtColor(cameraImage, imageGray, COLOR_BGRA2GRAY);
+
+                            RectVector detectedFace = new RectVector();
+                            cascade.detectMultiScale(imageGray, detectedFace, 1.1, 2, 0, new Size(150, 150), new Size(500, 500));
+
+                            for (int i = 0; i < detectedFace.size(); i++) {
+                                Rect dadosFace = detectedFace.get(i);
+                                rectangle(cameraImage, dadosFace, new Scalar(0, 255, 0, 3), 3, 0, 0);
+                                Mat faceCapturada = new Mat(imageGray, dadosFace);
+                                opencv_imgproc.resize(faceCapturada, faceCapturada, new Size(160, 160));
+
+                                IntPointer rotulo = new IntPointer(1);
+                                DoublePointer confidence = new DoublePointer(1);
+                                recognizer.predict(faceCapturada, rotulo, confidence);
+                                int prediction = rotulo.get(0);
+                                String nome;
+                                nome = firstNamePerson;
+
+                                if (prediction == -1) {
+                                    rectangle(cameraImage, dadosFace, new Scalar(0, 0, 255, 3), 3, 0, 0);
+                                    idPerson = 0;
+                                    facebook = "";
+                                    insta = "";
+                                    git = "";
+                                    linkedin = "";
+                                } else {
+                                    rectangle(cameraImage, dadosFace, new Scalar(0, 255, 0, 3), 3, 0, 0);
+                                    System.out.println(confidence.get(0));
+                                    idPerson = prediction;
+//                                    System.out.println("PESSOA RECONHECIDA COMO: " + idPerson);
+                                    rec();
+                                }
+                            }
+
+                            imencode(".bmp", cameraImage, mem);
+                            Image im = ImageIO.read(new ByteArrayInputStream(mem.getStringBytes()));
+                            BufferedImage buff = (BufferedImage) im;
+
+                            try {
+                                if (g.drawImage(buff, 0, 0, 360, 390, 0, 0, buff.getWidth(), buff.getHeight(), null)) {
+                                    if (runnable == false) {
+                                        this.wait();
+                                    }
+                                }
+                            } catch (Exception e) {
+                            }
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }
+    }
+
+    private void rec() {
+        //Retrieve data from database
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    
+                } catch (Exception e) {
+                }
+            }
+        }.start();
+    }
+
+    public void stopCamera() {
+        try {
+            myThread.runnable = false;
+            webSource.release();
+            dispose();
+        } catch (Exception e) {
+        }
+    }
+
+    public void startCamera() {
+        new Thread() {
+            @Override
+            public void run() {
+                webSource = new VideoCapture(0);
+                myThread = new RecognizeFace.DaemonThread();
+                Thread t = new Thread(myThread);
+                t.setDaemon(true);
+                myThread.runnable = true;
+                t.start();
+            }
+        }.start();
+    }
+}
